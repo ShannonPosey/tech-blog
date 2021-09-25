57bd05dc59939dbbb2034306458629375bec29b3
shannon_p <snp1284kasey@aol.com>
2021-09-25 12:29:52 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+// install the following:
+// npm install
+// - init -y
+// - express-handlebars
+// - mysql2
+// - sequelize
+// - express
+// - dotenv
+// - bcrypt
+// - express-session
+// - connect-session-sequelize
