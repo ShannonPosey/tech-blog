15ac1a2e56817425c0f85b0e72fafea96a6b5405
shannon_p <snp1284kasey@aol.com>
2021-09-26 13:56:00 -0400

    added models

--- server.js
+++ server.js
@@ -4,8 +4,44 @@ const sequelize = require("./config/connection");
 const exphbs = require("express-handlebars");
 const session = require("express-session");
 
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
+const sess = {
+    secret: "placeholder",
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
+const app = express();
+const PORT = process.env.PORT || 3001 ;
+
+const routes = require("./controllers");
+
+const helpers = require("./utils/heplers");
+
+const hbs = exphbs.create({helpers});
+
+// Handlebar Template engine
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// turn on routes
+app.use(routes);
+
+sequelize.sync({force: false}).then(() => {
+    app.listen(PORT, () => console.log("App is now listening"));
+});
 
 
 
